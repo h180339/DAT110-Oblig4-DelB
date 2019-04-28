1c489f07e8d088a9399fb96808a882d48c2b6e11
h180339 <h@g.gov>
2019-04-28 11:59:27 +0200

    Step 2-3 ferdig

--- ACCloudService/src/main/java/no/hvl/dat110/ac/rest/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/rest/App.java
@@ -9,6 +9,8 @@ import static spark.Spark.delete;
 
 import com.google.gson.Gson;
 
+import java.lang.reflect.Type;
+
 /**
  * Hello world!
  *
@@ -39,12 +41,49 @@ public class App {
 		get("/accessdevice/hello", (req, res) -> {
 			
 		 	Gson gson = new Gson();
-		 	
-		 	return gson.toJson("IoT Access Control Device");
+
+		 	return gson.toJson("im the best");
 		});
 		
 		// TODO: implement the routes required for the access control service
-		
+		//lagrer
+		post("/accessdevice/log/", (request, response) -> {
+			Gson gson = new Gson();
+			AccessMessage body = gson.fromJson(request.body(), AccessMessage.class);
+			int id = accesslog.add(body.getMessage());
+			AccessEntry accessEntry = accesslog.get(id);
+			//System.out.println(accessEntry.getId() + " " + accessEntry.getMessage());
+			//System.out.println(accesslog.toJson());
+			response.body(gson.toJson(accessEntry));
+			return gson.toJson(accessEntry);
+		});
+
+		//viser alt i accesslog
+		get("/accessdevice/log/", (req, res) -> accesslog.toJson());
+
+		get("/accessdevice/log/:id", (req, res) -> {
+			Gson gson = new Gson();
+			Integer id = Integer.parseInt(req.params(":id"));
+			AccessEntry accessEntry = accesslog.get(id);
+			return gson.toJson(accessEntry);
+		});
+		//oppdaterer tilgangskoden
+		put("/accessdevice/code", (request, response) -> {
+			Gson gson = new Gson();
+			AccessCode newAccesscode = gson.fromJson(request.body(), AccessCode.class);
+			accesscode.setAccesscode(newAccesscode.getAccesscode());
+			response.body(gson.toJson(newAccesscode.getAccesscode()));
+			//System.out.println(gson.toJson(newAccesscode.getAccesscode()));
+			return gson.toJson(newAccesscode.getAccesscode());
+		});
+		get("/accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+			return gson.toJson(accesscode.getAccesscode());
+		});
+		delete("/accessdevice/log/", ((request, response) -> {
+			accesslog.clear();
+			return accesslog.toJson();
+		}));
     }
     
 }
