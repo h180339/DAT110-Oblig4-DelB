26ae684935617e92c850d8a0f7659e079a6c5481
Eirik <180339@stud.hvl.no>
2019-04-29 13:45:32 +0200

    doPostAccessEntry(); test funker

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -19,11 +19,67 @@ public class RestClient {
     private static int port = 8080;
     private static String host = "localhost";
     private String getURI = "/accessdevice/code";
+    private String postUri = "/accessdevice/log/";
 
     public void doPostAccessEntry(String message) {
 
         // TODO: implement a HTTP POST on the service to post the message
+        try (Socket s = new Socket(host, port)) {
 
+            // construct the HTTP request
+            Gson gson = new Gson();
+            AccessMessage accessMessage = new AccessMessage(message);
+            String jsonbody = gson.toJson(accessMessage);
+            //System.out.println("JSON BODY::" + jsonbody);
+
+            String httpputrequest =
+                    "POST " + postUri + " HTTP/1.1\r\n" +
+                            "Host: " + host + "\r\n" +
+                            "Content-type: application/json\r\n" +
+                            "Content-length: " + jsonbody.length() + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n" +
+                            jsonbody +
+                            "\r\n";
+
+            // send the response over the TCP connection
+            OutputStream output = s.getOutputStream();
+
+            PrintWriter pw = new PrintWriter(output, false);
+            pw.print(httpputrequest);
+            pw.flush();
+
+            // read the HTTP response
+            InputStream in = s.getInputStream();
+
+            Scanner scan = new Scanner(in);
+            StringBuilder jsonresponse = new StringBuilder();
+            boolean header = true;
+
+            while (scan.hasNext()) {
+
+                String nextline = scan.nextLine();
+
+                if (header) {
+                    //System.out.println(nextline);
+                } else {
+                    jsonresponse.append(nextline);
+                }
+
+                if (nextline.isEmpty()) {
+                    header = false;
+                }
+
+            }
+
+            System.out.println("BODY:");
+            System.out.println(jsonresponse.toString());
+
+            scan.close();
+
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
     }
 
     private static String codepath = "/accessdevice/code";
