bd94c94ea77bf20d66e393498baaa64a169e67d4
h180339 <h@g.gov>
2019-05-04 13:48:18 +0200

    4,5,6 + litt fiksing av koden

--- ACCloudService/src/main/java/no/hvl/dat110/ac/rest/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/rest/App.java
@@ -43,7 +43,7 @@ public class App {
 			
 		 	Gson gson = new Gson();
 
-		 	return gson.toJson("im the best");
+		 	return gson.toJson("testing");
 		});
 		
 		// TODO: implement the routes required for the access control service
@@ -53,8 +53,6 @@ public class App {
 			AccessMessage body = gson.fromJson(request.body(), AccessMessage.class);
 			int id = accesslog.add(body.getMessage());
 			AccessEntry accessEntry = accesslog.get(id);
-			//System.out.println(accessEntry.getId() + " " + accessEntry.getMessage());
-			//System.out.println(accesslog.toJson());
 			response.body(gson.toJson(accessEntry));
 			return gson.toJson(accessEntry);
 		});
@@ -74,22 +72,14 @@ public class App {
 			AccessCode newAccesscode = gson.fromJson(request.body(), AccessCode.class);
 			accesscode.setAccesscode(newAccesscode.getAccesscode());
 			response.body(gson.toJson(newAccesscode.getAccesscode()));
-			//System.out.println(gson.toJson(newAccesscode.getAccesscode()));
+
 			return gson.toJson(newAccesscode.getAccesscode());
 		});
 		get("/accessdevice/code", (req, res) -> {
 			//fikk ikke til å få fromJson toJson uten å fikse strengen manuelt, så ser forferdelig ut, men funker
 			Gson gson = new Gson();
-			String kode = "";
-			for (int i = 0; i < accesscode.getAccesscode().length; i++) {
-				kode += accesscode.getAccesscode()[i] + ",";
-			}
-			kode = kode.substring(0, kode.length() - 1);
-			String jsonString = "{\"accesscode\": [" + kode + "]}";
-
-			AccessCode accessCode = gson.fromJson(jsonString, AccessCode.class);
-			res.body(gson.toJson(accessCode));
-			return gson.toJson(accessCode);
+			res.body(gson.toJson(accesscode));
+			return gson.toJson(accesscode);
 		});
 		delete("/accessdevice/log/", ((request, response) -> {
 			accesslog.clear();
